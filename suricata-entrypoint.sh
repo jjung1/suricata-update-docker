ea3ba2aecf81b6a28feaf3ed0ae5477dc34bb6b9
jjung1 <35614012+jjung1@users.noreply.github.com>
2018-07-30 14:07:06 -0400

    Create suricata-entrypoint.sh

---
+++ suricata-entrypoint.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# Suricata entrypoint script
+
+set -e
+
+if [ ! -d "/logs/suricata" ]; then
+  mkdir -p /logs/suricata
+fi
+
+sleep 10
+
+# Start Suricata normally
+suricata -c /etc/suricata/suricata.yaml --af-packet
